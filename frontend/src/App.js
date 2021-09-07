bc8c5005b44ad61c4d172f520b2f43a9e9d830de
OktayMehmed <oktay888@abv.bg>
2021-09-07 19:55:39 +0300

    product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap'
-import Header from './components/header'
-import Footer from './components/footer'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './components/HomeScreen'
 
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>iStyle</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
