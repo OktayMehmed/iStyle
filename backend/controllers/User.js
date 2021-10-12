b5e9e959d6840801ae1843ce7587403cf185f1a2
OktayMehmed <oktay888@abv.bg>
2021-10-12 23:30:54 +0300

    generate JSON Web Token

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,4 +1,5 @@
 const User = require("../model/User");
+const generateToken = require("../utils/jwt");
 
 // @desc Auth user & get token
 // @route POST /api/users/login
@@ -7,19 +8,22 @@ const authUser = (req, res, next) => {
   const { email, password } = req.body;
 
   User.findOne({ email })
-    .then((user) => Promise.all([user, user.matchPassword(password)]))
-    .then(([user, match]) => {
-      if (user && match) {
-        res.json({
-          _id: user._id,
-          name: user.name,
-          email: user.email,
-          isAdmin: user.isAdmin,
-          token: null,
-        });
-      } else {
-        res.status(401).json({ message: "Invalid email or password" });
-      }
-    }).catch(() => res.status(401).json({ message: "Invalid email or password" }))
+  .then((user) => Promise.all([user, user.matchPassword(password)]))
+  .then(([user, match]) => {
+    if (user && match) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  })
+  .catch(() =>
+    res.status(401).json({ message: "Invalid email or password" })
+  );
 };
 module.exports = { authUser };
