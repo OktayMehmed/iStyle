d67c22ac5479f455563dee9e960583ea799f31b7
OktayMehmed <oktay888@abv.bg>
2021-10-15 19:22:52 +0300

    implement update user controller and route

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -39,21 +39,21 @@ const registerUser = (req, res, next) => {
     }
   });
 
-  User.create({ name, email, password }).then((user) => {
-    if (user) {
-      res.status(201).json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generateToken(user._id),
-      });
-    } else {
-      res.status(400)
-      .json({ message: "Invalid user data" });
-    }
-    
-  }).catch((e) => res.status(400).json(e))
+  User.create({ name, email, password })
+    .then((user) => {
+      if (user) {
+        res.status(201).json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user._id),
+        });
+      } else {
+        res.status(400).json({ message: "Invalid user data" });
+      }
+    })
+    .catch((e) => res.status(400).json(e));
 };
 
 // @desc Get user profile
@@ -69,9 +69,35 @@ const getUserProfile = (req, res, next) => {
         isAdmin: user.isAdmin,
       });
     } else {
-      res.status(404);
-      throw new Error("User not found");
+      res.status(404).json({ message: "User not found" });
     }
   });
 };
-module.exports = { authUser, getUserProfile, registerUser };
+
+// @desc Update user profile
+// @route PUT /api/users/profile
+// @access Private
+const updateUserProfile = (req, res, next) => {
+  User.findById(req.user._id).then((user) => {
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = req.body.password;
+      }
+
+      user.save().then((updatedUser) => {
+        res.json({
+          _id: updatedUser._id,
+          name: updatedUser.name,
+          email: updatedUser.email,
+          isAdmin: updatedUser.isAdmin,
+          token: generateToken(updatedUser._id),
+        });
+      });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  });
+};
+module.exports = { authUser, getUserProfile, registerUser, updateUserProfile };
--- backend/routes/User.js
+++ backend/routes/User.js
@@ -4,11 +4,12 @@ const {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile
 } = require("../controllers/User");
 const { protect } = require("../middleware/auth");
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile)
 
 module.exports = router;
