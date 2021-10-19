5df7e864402dce1ce564a94380bcb08002c40181
OktayMehmed <oktay888@abv.bg>
2021-10-20 01:17:48 +0300

    Order details reducer and action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -34,3 +34,37 @@ export const createOrder = (order) => (dispatch, getState) => {
       });
     });
 };
+
+export const getOrderDetails = (id) => (dispatch, getState) => {
+  dispatch({
+    type: "ORDER_DETAILS_REQUEST",
+  });
+
+  const {
+    userLogin: { userInfo },
+  } = getState();
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${userInfo.token}`,
+    },
+  };
+
+  axios
+    .get(`/api/orders/${id}`, config)
+    .then(({ data }) => {
+      dispatch({
+        type: "ORDER_DETAILS_SUCCESS",
+        payload: data,
+      });
+    })
+    .catch((error) => {
+      dispatch({
+        type: "ORDER_DETAILS_FAIL",
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    });
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,7 +12,10 @@ import {
   userDetailsReducer,
   updateProfileReducer,
 } from "./reducers/userReducers";
-import { orderCreateReducer } from "./reducers/orderReducers";
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+} from "./reducers/orderReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -23,6 +26,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: updateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
