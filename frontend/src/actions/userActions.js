6716bd2c057298824c9a43788ff3cd35f410f3e1
OktayMehmed <oktay888@abv.bg>
2021-10-24 20:56:26 +0300

    Admin page security

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -37,6 +37,7 @@ export const logout = () => (dispatch) => {
   dispatch({ type: "USER_DETAILS_RESET" });
   dispatch({ type: "ORDER_MY_LIST_RESET" });
   dispatch({ type: "CART_RESET" });
+  dispatch({ type: "USER_LIST_RESET" });
 };
 
 export const register = (name, email, password) => (dispatch) => {
@@ -178,4 +179,3 @@ export const listUsers = () => (dispatch, getState) => {
       });
     });
 };
-
--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import {logout} from '../../actions/userActions'
+import { logout } from "../../actions/userActions";
 import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
+  const dispatch = useDispatch();
 
-	const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
 
-	const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-	const { userInfo } = userLogin
-
-
-	const logoutHandler = () => {
-		dispatch(logout());
-	}
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -31,30 +29,34 @@ const Header = () => {
                   <i className="fas fa-shopping-cart"></i> Cart
                 </Nav.Link>
               </LinkContainer>
-							{userInfo ? (
-								<NavDropdown title={userInfo.name} id='username'>
-									<LinkContainer to='/profile'>
-										<NavDropdown.Item>Profile</NavDropdown.Item>
-									</LinkContainer>
-									<NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
-								</NavDropdown>
-							) : <LinkContainer to="/login">
-							<Nav.Link>
-								<i className="fas fa-sign-in-alt"></i> Login
-							</Nav.Link>
-						</LinkContainer>}
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <i className="fas fa-sign-in-alt"></i> Login
+                  </Nav.Link>
+                </LinkContainer>
+              )}
               {userInfo && userInfo.isAdmin && (
-                <NavDropdown title='Admin' id='adminmenu'>
-                <LinkContainer to='/admin/userlist'>
-                  <NavDropdown.Item>Users</NavDropdown.Item>
-                </LinkContainer>
-                <LinkContainer to='/admin/productlist'>
-                  <NavDropdown.Item>Products</NavDropdown.Item>
-                </LinkContainer>
-                <LinkContainer to='/admin/orderlist'>
-                  <NavDropdown.Item>Orders</NavDropdown.Item>
-                </LinkContainer>
-              </NavDropdown>
+                <NavDropdown title="Admin" id="adminmenu">
+                  <LinkContainer to="/admin/userlist">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/productlist">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/orderlist">
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
               )}
             </Nav>
           </Navbar.Collapse>
