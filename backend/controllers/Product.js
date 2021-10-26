da0c664b228c9ecb8e8e318277b645ebdba8c5db
OktayMehmed <oktay888@abv.bg>
2021-10-26 20:39:00 +0300

    Create and Update Product controller

--- backend/controllers/Product.js
+++ backend/controllers/Product.js
@@ -22,12 +22,60 @@ const getProductById = (req, res) => {
 const deleteProduct = (req, res) => {
   Product.findById(req.params.id)
     .then((product) => {
-      if(product) {
-        product.remove()
-        res.json({ message: 'Product was been deleted' })
+      if (product) {
+        product.remove();
+        res.json({ message: "Product was been deleted" });
       }
     })
     .catch(() => res.status(404).json({ message: "Product not find!" }));
 };
 
-module.exports = { getProducts, getProductById, deleteProduct };
+// @desc Create a product
+// @route POST /api/products
+// @access Private/Admin
+const createProduct = (req, res) => {
+  const product = new Product({
+    name: "Sample",
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    description: "Sample desc",
+    price: 10,
+    countInStock: 10,
+    numReviews: 0,
+  });
+
+  product.save().then((createdProduct) => {
+    res.status(201).json(createdProduct);
+  });
+};
+
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+const updateProduct = (req, res) => {
+  const { name, image, description, price, countInStock } = req.body;
+
+  Product.findById(req.params.id).then((product) => {
+    if (product) {
+      (product.name = name),
+        (product.image = image),
+        (product.description = description),
+        (product.price = price),
+        (product.countInStock = countInStock)
+
+        product.save().then((updatedProduct) => {
+          res.json(updatedProduct);
+        });
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  });
+};
+
+module.exports = {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
--- backend/routes/Product.js
+++ backend/routes/Product.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { getProducts, getProductById, deleteProduct } = require("../controllers/Product");
-const { protect, isAdmin } = require('../middleware/auth')
+const {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+} = require("../controllers/Product");
+const { protect, isAdmin } = require("../middleware/auth");
 
-router.route("/").get(getProducts);
-router.route("/:id").get(getProductById).delete(protect, isAdmin, deleteProduct)
+router.route("/").get(getProducts).post(protect, isAdmin, createProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, isAdmin, updateProduct)
+  .delete(protect, isAdmin, deleteProduct);
 
 module.exports = router;
