2873b9bfb72fa2a0345eabda6444fc38fad8f6d7
Oktay Mehmed <oktay888@abv.bg>
2022-02-02 19:04:05 +0200

    header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import { Container } from "react-bootstrap";
 import { BrowserRouter, Route } from "react-router-dom";
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/header";
+import Footer from "./components/footer";
 import HomePage from "./components/HomePage";
 import ProductDetail from "./components/ProductDetail";
 import CartPage from "./components/CartPage";
